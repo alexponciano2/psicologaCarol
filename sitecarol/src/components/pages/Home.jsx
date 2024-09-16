bd85022c1e5043cf90e92f9585524e6102747d35
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-09-16 20:14:12 -0300

    Update Home.jsx

--- sitecarol/src/components/pages/Home.jsx
+++ sitecarol/src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <section className={styles.home_container}>
       <h1>
-        Bem-vindo ao <span>Costs</span>
+        Psicologia com <span>Carol Reis</span>
       </h1>
       <p>Comece a gerenciar os seus projetos agora mesmo!</p>
     </section>
