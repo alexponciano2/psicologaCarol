44070950f0b7889b6f3402b73f2f4d86590d7b0d
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-09-16 21:20:42 -0300

    Update AboutMe.jsx

--- sitecarol/src/components/pages/AboutMe.jsx
+++ sitecarol/src/components/pages/AboutMe.jsx
@@ -1,5 +1,15 @@
-function AboutMe(){
-    return <h1>Sobre Mim</h1>
+import homecarol from '../../img/Carolina2.png'
+import styles from '../pages/AboutMe.module.css'
+
+function AboutMe() {
+    return (
+        <div className="container">
+            <div className="image-side">
+                <img src={homecarol} alt="carol" className={styles.imagem}></img>
+            </div>
+            <div className="gradient-side"></div>
+        </div>
+    )
 }
 
 export default AboutMe
