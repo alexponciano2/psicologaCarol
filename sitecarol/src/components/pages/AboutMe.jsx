380cd13223439f5d2ff4e3f83aa4423b8c2e0337
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-09-18 20:22:20 -0300

    Update AboutMe.jsx

--- sitecarol/src/components/pages/AboutMe.jsx
+++ sitecarol/src/components/pages/AboutMe.jsx
@@ -1,15 +1,12 @@
-import homecarol from '../../img/Carolina2.png'
+import homecarol from '../../img/Carolina3.png'
 import styles from '../pages/AboutMe.module.css'
 
 function AboutMe() {
     return (
         <div className={styles.container}>
-            <div className={styles.imageside}>
-                <img src={homecarol} alt="carol" className={styles.imagem}></img>
-                <div class={styles.overlaygradient}></div>
-            </div>
-            <div className={styles.gradientside}></div>
+            <img src={homecarol} alt="Imagem"></img>
         </div>
+
     )
 }
 
