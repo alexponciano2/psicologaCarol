93a254bd097ac595777214973a4bc937cfcfd92a
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2024-09-21 15:37:00 -0300

    update

--- sitecarol/src/components/pages/AboutMe.jsx
+++ sitecarol/src/components/pages/AboutMe.jsx
@@ -1,19 +1,11 @@
-import homecarol from '../../img/Carolina6.png';
 import React from 'react';
 import Container from '../layout/Container';
-import styles from '../layout/Container.module.css';
+import styles from '../pages/AboutMe.module.css';
 
 function AboutMe() {
   return (
-    <Container customClass="start">
-      <div className={styles.textinho}>
-        Atendimento 100% on-line (ou presencial na Tijuca/RJ)
-        Uma escuta acolhedora, atenta, interessada e cuidadosa
-        Um espaço seguro e livre de julgamentos
-        Abordagem fenomenológico-existencial
-        Adolescentes, adultos e idosos
-      </div>
-      <img src={homecarol} alt="Imagem" className={styles['full-width-image']} />
+    <Container customClass="start">      
+      <div className={styles['background-image']}></div> {/* Aplicando a classe via CSS Modules */}
     </Container>
   );
 }
